20b94edec97f4cb102282f6a4cbb8eefb5f59076
devover <the3cube@gmail.com>
2015-11-29 07:29:26 +0700

    EMAK first upload

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+  <div class="container">
+  	<div class="row">
+        <h2><i class="fa fa-paper-plane"></i><span>EMAK : portal-nya perempuan kreatif indonesia!</span></h2>
+      	<small>TENTANG KAMI | VISI | MISI | HUBUNGI KAMI</small>
+      </div>
+    </div>
+</footer>
---
+++ resources/views/user-invoice.blade.php
@@ -0,0 +1,54 @@
+  @extends('layouts.main')
+
+  @section('dataUtama')
+  <!--  Display featured project -->
+        <section class="portfolio text-center section-user-menu" id="user-page">
+          <div class="container">
+            <div class="row">
+               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <h1 class="arrow">Selamat datang, Astrid </h1>
+                 <p>EDIT PROFIL | INVOICE | SETTING</p>
+                 <hr/>
+        </section>
+
+        <section class="features section-user" id="user-page">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h3>DATA INVOICE PROJECT <b>TOPI KOPI</b></h3>
+                <table class="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>No.</th><th>No INVOICE</th><th>Tgl Invoice</th><th>PAKET</th><th>Keterangan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr><td>1</td><td>IN-6823</td><td>12-12-2015</td><td>PAKET A</td><td>Diproses</td></tr>
+                    <tr><td>2</td><td>IN-6827</td><td>12-12-2015</td><td>PAKET A</td><td>Diterima</td></tr>
+                    <tr><td>3</td><td>IN-6890</td><td>13-12-2015</td><td>PAKET B</td><td>Diterima</td></tr>
+                    <tr><td>4</td><td>IN-6891</td><td>13-12-2015</td><td>PAKET C</td><td>Diproses</td></tr>
+                    <tr><td>5</td><td>IN-6892</td><td>14-12-2015</td><td>PAKET A</td><td>Diproses</td></tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h3>DATA INVOICE PROJECT <b>SEDOTAN ULAR</b></h3>
+                <table class="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>No.</th><th>No INVOICE</th><th>Tgl Invoice</th><th>PAKET</th><th>Keterangan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr><td>1</td><td>IN-6823</td><td>12-12-2015</td><td>PAKET A</td><td>Diproses</td></tr>
+                    <tr><td>2</td><td>IN-6827</td><td>12-12-2015</td><td>PAKET A</td><td>Diterima</td></tr>
+                    <tr><td>3</td><td>IN-6890</td><td>13-12-2015</td><td>PAKET B</td><td>Diterima</td></tr>
+                    <tr><td>4</td><td>IN-6891</td><td>13-12-2015</td><td>PAKET C</td><td>Diproses</td></tr>
+                    <tr><td>5</td><td>IN-6892</td><td>14-12-2015</td><td>PAKET A</td><td>Diproses</td></tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+           </div>
+         </section>
+  @stop
