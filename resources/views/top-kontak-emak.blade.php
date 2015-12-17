0ad9b2d5f5fdd814d6258770291f114d586fcb62
devover <the3cube@gmail.com>
2015-12-17 11:51:29 +0700

    add menu page

---
+++ resources/views/top-kontak-emak.blade.php
@@ -0,0 +1,30 @@
+  @extends('layouts.main')
+
+  @section('dataUtama')
+  <!--  Display featured project -->
+        <section class="portfolio text-center section-user-menu" id="user-page">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                 <hr/>
+                  <h1 class="arrow">Kontak </h1>
+                 <hr/>
+              </div>
+            </div>
+        </section>
+
+        <section class="features section-user" id="user-page">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.              
+              </div>
+            </div>
+           </div>
+         </section>
+  @stop
