13d4de8b24c1f7d52eba1fffbd41258b4dbf81df
devover <the3cube@gmail.com>
2015-12-20 21:37:17 +0700

    add route+foot+fix menu+styling

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,14 +15,26 @@ Route::get('/', function () {
     return View::make("welcome");
 });
  
-Route::get('/pita-hias', function () {
+// Top menu
+Route::get('/about-emak', function () {
+    return view('top-about-emak');
+});
+Route::get('/proyek-emak', function () {
+    return view('top-proyek-emak');
+});
+Route::get('/visi-misi-emak', function () {
+    return view('top-visi-misi-emak');
+});
+Route::get('/kontak', function () {
+    return view('top-kontak-emak');
+});
+// sample page
+ Route::get('/pita-hias', function () {
     return view('pita-hias');
 });
-
 Route::get('/user-profile', function () {
     return view('user-profile');
 });
-
 Route::get('/user-invoice', function () {
     return view('user-invoice');
 });
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,11 @@
   <div class="container">
   	<div class="row">
         <h2><i class="fa fa-paper-plane"></i><span>EMAK : portal-nya perempuan kreatif indonesia!</span></h2>
-      	<small>TENTANG KAMI | VISI | MISI | HUBUNGI KAMI</small>
+      	<small>
+      	<a href="{{ Request::root() }}/about-emak">TENTANG KAMI</a>
+      	| <a href="{{ Request::root() }}/visi-misi-emak">VISI & MISI</a>
+      	| <a href="{{ Request::root() }}/kontak">HUBUNGI KAMI</a>
+      	</small>
       </div>
     </div>
 </footer>
