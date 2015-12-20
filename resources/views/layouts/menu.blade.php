ea4ff10bf2c797047df5b4b73ba749a7d86e9fb6
devover <the3cube@gmail.com>
2015-12-21 01:11:13 +0700

    update menus and profile

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,7 +9,7 @@
               
               <!-- Collect the nav links, forms, and other content for toggling -->
               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right ">
+                <ul class="nav navbar-nav navbar-right" style="margin-top: 25px;">
                   <li class="hidden"> <a href="#page-top"></a> </li>
                   <li> <a class="page-scroll" href="{{ Request::root() }}/about-emak">Apa itu EMAK?</a> </li>
                   <li> <a class="page-scroll" href="{{ Request::root() }}/proyek-emak">Proyek</a> </li>
--- resources/views/top-about-emak.blade.php
+++ resources/views/top-about-emak.blade.php
@@ -5,26 +5,67 @@
         <section class="portfolio text-center section-user-menu" id="user-page">
           <div class="container">
             <div class="row">
-              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                 <hr/>
-                  <h1 class="arrow">Apa itu emak? </h1>
-                 <hr/>
-              </div>
-            </div>
+               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <hr/>
+                  <h1 class="arrow">Apa itu EMAK? </h1>
+                  <!-- <p>EDIT PROFIL | INVOICE | SETTING</p> -->
+                  <hr/>
         </section>
 
         <section class="features section-user" id="user-page">
           <div class="container">
             <div class="row">
-              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="min-height: 270px;">
-				<p>EMAK (kata panggilan lain untuk ibu) adalah portal yang mewadahi ide kreatif perempuan dalam membuat produk kerajinan tangan.  Kerajinan tangan banyak sekali jenisnya, alat dan bahannya pun bisa dari barang dasar tertentu maupun barang bekas. Tingkat kesulitan pembuatan kerajinan tangan pun bermacam-macam, ada yang mudah sekali sampai rumit dalam pembuatannya.</p>
-				<p>Saat ini banyak perempuan-perempuan Indonesia yang kreatif di bidang kerajinan, namun tidak maksimal dalam pengelolaan hasil yang didapatkan.  Untuk membantu memaksimalkan hasil  yang didapatkan,  dibutuhkan pengemasan ataupun iklan yang semenarik mungkin. Sehingga produk yang ditampilkan akan menarik investor(user) untuk berinvestasi/berdonasi terhadap produk yang dibuat. </p>
-				<p>EMAK mengajak perempuan-perempuan kreatif Indonesia untuk berkreasi menciptakan ide-ide kreatif yang bermanfaat yang akan didukung oleh EMAK. </p>
-				<p>Mari bergabung bersama EMAK.</p>
-	
-
-							 
+              <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <h1 >EMAK?</h1>
+                        <p>EMAK (kata panggilan lain untuk ibu) adalah portal yang mewadahi ide kreatif perempuan dalam membuat produk kerajinan tangan yang akan menarik investor(user) untuk berinvestasi/berdonasi terhadap produk yang dibuat.</p>
+                        <p>Saat ini banyak perempuan-perempuan Indonesia yang kreatif di bidang kerajinan, namun tidak maksimal dalam pengelolaan hasil yang didapatkan.</p>
+                        <p>Kami mengajak perempuan-perempuan kreatif Indonesia untuk menciptakan ide-ide kreatif yang akan didukung oleh EMAK.</p>
               </div>
+              <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <h1 >Bagaimana EMAK Bekerja?</h1>
+                        <div class="col-md-12">
+                            <div class="service-sec">
+                                
+                                  <div class="icon">
+                                    <i class="fa fa-user"></i>
+                                  </div>
+                                  
+                                  <div class="detail text-left">
+                                    
+                                      <p>Author (user) membuat kerajinan yang akan dipublish ke Emak, lalu sistem akan menampilkan produk kerajinan.</p>
+                                  </div>
+                                  
+                              </div>
+                         </div>
+                         <div class="col-md-12">
+                            <div class="service-sec">
+                                
+                                  <div class="icon">
+                                    <i class="fa fa-users"></i>
+                                  </div>
+                                  
+                                  <div class="detail text-left">
+                                    
+                                      <p>Investor/donator akan melihat hasil kerajinan, memberikan donasi. Jumlah donasi yang diberikan oleh donator akan menentukan apa yang akan mereka dapatkan.</p>
+                                  </div>
+                                  
+                              </div>
+                         </div>
+                         <div class="col-md-12">
+                            <div class="service-sec">
+                                
+                                  <div class="icon">
+                                    <i class="fa fa-money"></i>
+                                  </div>
+                                  <div class="detail text-left">
+                                    
+                                      <p>Jumlah donasi satu produk yang didapatkan author akan diakumulasi. Hasil donasi akan dibagi 25% untuk EMAK untuk pengelolaan sistem, 75% untuk author untuk pembuatan dan pengembangan produk.</p>
+                                  </div>
+                                  
+                              </div>
+                         </div>
+              </div>
+
             </div>
            </div>
          </section>
